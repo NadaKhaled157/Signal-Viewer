355ba93f7df71549e11732da1629b6e88b03c97f
NadaKhaled157 <nadakhaled9277@gmail.com>
2024-10-11 02:31:42 +0300

    Live & Polar Signal Updates

--- del/test2.py
+++ del/test2.py
@@ -1,46 +1,61 @@
 import sys
 import time
-import numpy as np
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from unofficial_livecounts_api.youtube import YoutubeAgent
 import pyqtgraph as pg
 
-class DataThread(QThread):
-    data_signal = pyqtSignal(np.ndarray)
+
+class FetchData(QThread):
+    signal = pyqtSignal(float)
+
+    def __init__(self):
+        super().__init__()
+        self.connected = False
+        self.agent = YoutubeAgent()
+        self.channel_id = "UCX6OQ3DkcsbYNE6H8uQQuVA"  # MrBeast Channel
 
     def run(self):
-        while True:
-            data = np.random.normal(size=100)  # Simulate data update
-            self.data_signal.emit(data)
-            time.sleep(0.1)  # Simulate delay
+        while self.connected:
+            try:
+                metrics = self.agent.fetch_channel_metrics(query=self.channel_id)
+                subscriber_count = metrics.follower_count
+                print(f"Live Subscriber Count for channel MrBeast: {subscriber_count}")
+                self.signal.emit(subscriber_count)
+                time.sleep(3)
+            except Exception as e:
+                print(f"Error fetching data: {e}")
+                break
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.fetch_count = FetchData()
+        self.fetch_count.signal.connect(self.update_count)
+        self.init_ui()
 
-    def initUI(self):
-        self.setWindowTitle("Real-Time Plotting with PyQtGraph")
-        self.resize(800, 600)
+    def init_ui(self):
+        self.setGeometry(100, 100, 1400, 800)
+        self.setWindowTitle("YouTube Live Subscriber Count")
 
-        self.plot_widget = pg.PlotWidget()
-        self.plot_data = self.plot_widget.plot()
+        self.plot_widget = pg.PlotWidget(self)
+        self.plot_widget.setGeometry(50, 100, 1300, 600)
+        self.plot_widget.showGrid(x=True, y=True)
+        self.plot_widget.setLabel('left', 'Subscriber Count')
+        self.plot_widget.setLabel('bottom', 'Time')
+        self.data = []
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.plot_widget)
+        button = QPushButton("Connect", self)
+        button.setGeometry(1060, 2, 200, 50)
+        button.setStyle
 
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+def main():
+    app = QApplication(sys.argv)  # create new app object
+    window = MainWindow()  # create new window object
+    window.show()
+    sys.exit(app.exec_())  # app.exec_() waits for user to close window
 
-        self.data_thread = DataThread()
-        self.data_thread.data_signal.connect(self.update_plot)
-        self.data_thread.start()
 
-    def update_plot(self, data):
-        self.plot_data.setData(data)
-
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-sys.exit(app.exec_())
+if __name__ == "__main__":  # script is being run directly not imported
+    main()
--- main.py
+++ main.py
@@ -1,117 +1,72 @@
 import sys
-# import time
-# import requests
+import time
 import threading
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QLineEdit, QFileDialog
-from PyQt5.QtGui import QIcon, QFont, QPixmap
-from PyQt5.QtCore import Qt  # alignment
-
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import A4
-import pyautogui
-import pyscreenshot as ImageGrab
-import pandas as pd
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+from PyQt5.QtGui import QIcon
 import pyqtgraph as pg
-
-
 from LiveSignal import DataFetcher
 
-screen_width, screen_height = pyautogui.size()
+
+# import pandas as pd
+# import pyautogui
+# from reportlab.pdfgen import canvas
+# from reportlab.lib.pagesizes import A4
+# import pyscreenshot as ImageGrab
+
+# screen_width, screen_height = pyautogui.size()
 
 
-def take_snapshot(channel_number):
-    if channel_number == 1:
-        bbox = (380, 160, 1500, 535)
-        # signal_name = signal_one_name
-    elif channel_number == 2:
-        bbox = (380, 500, 1500, 1060)
-        # signal_name = signal_two_name
-    else:
-        bbox = (380, 160, 1500, 1070)
-
-    # snapshot = pyautogui.screenshot()
-    # snapshot_path = "Snapshots/snapshot.png"
-    # snapshot.save(snapshot_path)
-
-    snapshot = ImageGrab.grab(bbox=bbox)
-    snapshot.show()
-    snapshot_path = "Snapshots/snapshot.png"
-    snapshot.save(snapshot_path)
-    pdf = canvas.Canvas("Reports/example.pdf", pagesize=A4)
-    pdf.setTitle("Sample PDF")
-    # pdf.drawString(200, 800, "Hello, user!")
-    pdf.drawImage(snapshot_path, 70, 500, 350, 150)
-    pdf.save()
+# def take_snapshot(channel_number):  # IN PROGRESS
+#     if channel_number == 1:
+#         bbox = (380, 160, 1500, 535)
+#         # signal_name = signal_one_name
+#     elif channel_number == 2:
+#         bbox = (380, 500, 1500, 1060)
+#         # signal_name = signal_two_name
+#     else:
+#         bbox = (380, 160, 1500, 1070)
+#     # snapshot = pyautogui.screenshot()
+#     # snapshot_path = "Snapshots/snapshot.png"
+#     # snapshot.save(snapshot_path)
+#     snapshot = ImageGrab.grab(bbox=bbox)
+#     snapshot.show()
+#     snapshot_path = "Snapshots/snapshot.png"
+#     snapshot.save(snapshot_path)
+#     pdf = canvas.Canvas("Reports/example.pdf", pagesize=A4)
+#     pdf.setTitle("Sample PDF")
+#     # pdf.drawString(200, 800, "Hello, user!")
+#     pdf.drawImage(snapshot_path, 70, 500, 350, 150)
+#     pdf.save()
 
 
 class MainWindow(QMainWindow):  # inherits from QMainWindow
     def __init__(self):  # constructor
         super().__init__()  # calls parent init to ensure proper initialization
-        self.plot_widget = None
+        self.plot_widget_one = None
+        self.plot_widget_two = None
         self.thread = None
-        self.data_fetcher = DataFetcher()
-        self.data_fetcher.data_signal.connect(self.update_plot)
+        self.fetch_subscriber_count = DataFetcher()
+        self.fetch_subscriber_count.live_signal.connect(self.update_count)
+        self.timestamps = []
+        self.subscriber_counts = []
+        self.start_time = time.time()
+        self.init_ui()
 
+    def init_ui(self):
+
+        ####REPLACE WITH NEW UI#####
         self.setWindowTitle("BIOSIGNAL")
         self.setGeometry(0, 0, 1860, 950)
         self.setWindowIcon(QIcon("images/app icon.png"))
-        self.initUI()
-
-    def initUI(self):
-        menu_bar_label = QLabel(self)
-        menu_bar_label.setGeometry(0, 0, 1860, 55)
-        menu_bar_label.setStyleSheet("background-color: #f0f0f0;")
-
-        import_button = QPushButton("Imp", self)
-        import_button.setGeometry(5, 2, 50, 50)
-        import_button.setStyleSheet("background-color: #fafafa")
-        import_button.clicked.connect(self.import_signal)
-        # snapshot_button.setIcon(QIcon("import_icon.png"))
-
-        snapshot_button = QPushButton("1", self)
-        snapshot_button.setGeometry(60, 2, 50, 50)
-        snapshot_button.setStyleSheet("background-color: #fafafa")
-        snapshot_button.clicked.connect(lambda: take_snapshot(1))
-        # snapshot_button.setIcon(QIcon("snapshot_icon.png"))
-
-        # link_channels_button = QPushButton("Link Channels ", self)
-        # link_channels_button.setFont(QFont("Garet", 8))
-        # link_channels_button.setGeometry(115, 2, 200, 50)
-        # link_channels_button.setStyleSheet("background-color: #fafafa")
-        # link_channels_button.setStyleSheet("color:#3f446f;"
-        #                                   "background-color:white;"
-        #                                   "font-weight:bold;")
-        # link_channels_button.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
-        # label.setAlignment(Qt.AlignLeft)
-        # label.setAlignment(Qt.AlignVCenter)  # vertical alignment (default)
-        # label.setAlignment(Qt.AlignCenter)  # center vertically & horizontally
-
-        snapshot_two_button = QPushButton("2", self)
-        snapshot_two_button.setGeometry(320, 2, 50, 50)
-        snapshot_two_button.setStyleSheet("background-color: #fafafa")
-        snapshot_two_button.clicked.connect(lambda: take_snapshot(2))
-        # snapshot_button.setIcon(QIcon("snapshot_icon.png"))
-
-        left_section_label = QLabel(self)
-        left_section_label.setGeometry(15, 70, 300, 850)
-        left_section_label.setStyleSheet("background-color: #fafafa;")
-
-        middle_section_label = QLabel(self)
-        middle_section_label.setGeometry(330, 70, 1200, 850)
-        middle_section_label.setStyleSheet("background-color: #fafafa;")
-
-        right_section_label = QLabel(self)
-        right_section_label.setGeometry(1545, 70, 300, 850)
-        right_section_label.setStyleSheet("background-color: #fafafa;")
 
         channel_one_label = QLabel(self)
         channel_one_label.setGeometry(380, 120, 1100, 350)
         channel_one_label.setStyleSheet("background-color: #f5f5f5;"
                                         "border: 2px solid #212434")
         channel_one_layout = QtWidgets.QVBoxLayout(channel_one_label)
-        self.plot_widget = pg.PlotWidget()
-        channel_one_layout.addWidget(self.plot_widget)
+        self.plot_widget_one = pg.PlotWidget()
+        channel_one_layout.addWidget(self.plot_widget_one)
 
         channel_two_label = QLabel(self)
         channel_two_label.setGeometry(380, 520, 1100, 350)
@@ -120,105 +75,91 @@ class MainWindow(QMainWindow):  # inherits from QMainWindow
         channel_two_layout = QtWidgets.QVBoxLayout(channel_two_label)
         self.plot_widget_two = pg.PlotWidget()
         channel_two_layout.addWidget(self.plot_widget_two)
-        self.latitude_plot = self.plot_widget.plot(pen='r', name="Latitude")
-        self.longitude_plot = self.plot_widget.plot(pen='b', name="Longitude")
 
-        import_icon = QPixmap("import_icon.png")
-        # link_channels_button.setPixmap(import_icon)
-        # link_channels_button.setScaledContents(True)
-
-        self.signal_one_label = QLineEdit(self)
-        self.signal_one_label.setGeometry(100, 100, 200, 40)
-        self.signal_one_label.setStyleSheet("font-size: 20px;"
-                                            "font-family: Garet;")
-        signal_one_button = QPushButton("Update Label", self)
-        signal_one_button.setGeometry(100, 200, 200, 50)
-        signal_one_button.clicked.connect(self.update_label)
-
-        button = QPushButton("click meee ", self)
-        button.setGeometry(400, 2, 200, 50)
+        button = QPushButton("Subscriber", self)
+        button.setGeometry(1060, 2, 200, 50)
         button.setStyleSheet("font-size: 30px;")
-        button.clicked.connect(self.on_click)
+        button.clicked.connect(lambda: self.live_connection('connect'))
 
-        button = QPushButton("Connect Live", self)
-        button.setGeometry(620, 2, 200, 50)
+        button = QPushButton("no count", self)
+        button.setGeometry(1280, 2, 200, 50)
         button.setStyleSheet("font-size: 30px;")
-        button.clicked.connect(lambda: self.channel_two('connect'))
+        button.clicked.connect(lambda: self.live_connection('disconnect'))
 
-        button = QPushButton("Disconnect", self)
-        button.setGeometry(840, 2, 200, 50)
-        button.setStyleSheet("font-size: 30px;")
-        button.clicked.connect(lambda: self.channel_two('disconnect'))
+        ####REPLACE WITH NEW UI#####
 
-        self.data_fetcher.data_signal.connect(self.update_plot)
+        self.fetch_subscriber_count.live_signal.connect(self.update_count)
 
-    def channel_two(self, status):
-        if status == 'connect':
-            self.data_fetcher.connected = True
-            self.thread = threading.Thread(target=self.data_fetcher.fetch_data)
+    def live_connection(self, status):
+        if status == "connect":
+            self.fetch_subscriber_count.connected = True
+            self.thread = threading.Thread(target=self.fetch_subscriber_count.fetch_data)
             self.thread.start()
         else:
-            self.data_fetcher.connected = False
-            if self.data_fetcher.driver:
-                self.data_fetcher.driver.quit()
+            self.fetch_subscriber_count.connected = False
 
-    def update_plot(self):
-        self.plot_widget_two.plot(self.data_fetcher.latitude_data, self.data_fetcher.longitude_data, pen=pg.mkPen(color='r', width=2), symbol='o',
-                                  symbolBrush='b')
-
-    def update_label(self):
-        signal_one_name = self.signal_one_label.text()
-        print(f"Clicked {signal_one_name}")
-
-    # def on_click(self):
-    #     print("button clicked")
-    #     print(self.open_file())
-
-    # def open_file(self):
-    #     filename = QFileDialog.getOpenFileName()
-    #     path = filename[0]
-    #     print(path)
+    # def update_count(self):
+    #     subscriber_count = self.fetch_subscriber_count.subscriber_count
+    #     elapsed_time = time.time() - self.start_time  # Change to full time?
     #
-    #     x_data = []
-    #     y_data = []
+    #     self.timestamps.append(elapsed_time)
+    #     self.subscriber_counts.append(subscriber_count)
     #
-    #     file = open(path, "r")
-    #     read = file.readlines()
+    #     self.plot_widget_two.plot(self.timestamps, self.subscriber_counts, pen=pg.mkPen(color='g', width=2), symbol='o',
+    #                               symbolBrush='w')
     #
-    #     for line in read:
-    #         line = line.strip()
-    #         print(line)
-    #         if line:
-    #             x, y = line.split(',')
-    #             x_data.append(float(x))
-    #             y_data.append(float(y))
+    #     self.plot_widget_two.setLabel('left', 'Subscribers Count')
+    #     self.plot_widget_two.setLabel('bottom', 'Time')
     #
-    #     print ("x data:", x_data)
-    #     print("y data:", y_data)
-    #     return x_data, y_data
+    #     # Set Y-axis range
+    #     if self.subscriber_counts:
+    #         y_min = min(self.subscriber_counts) - 10
+    #         y_max = max(self.subscriber_counts) + 10
+    #         self.plot_widget_two.setYRange(y_min, y_max)
+    #         self.plot_widget_two.setXRange(0, max(self.timestamps) + 5)
+    #
+    #         # Format y-axis ticks
+    #         self.plot_widget_two.getAxis('left').setTicks(
+    #             [[(count, str(count)) for count in
+    #               range(int(y_min), int(y_max) + 1, 5)]])  # Difference of 5 subscribers
+    #
+    #     # Format x-axis to show time
+    #     self.plot_widget_two.getAxis('bottom').setTicks(
+    #         [[(t, time.strftime("%H:%M:%S", time.gmtime(t))) for t in self.timestamps]])
+    #
+    #     self.plot_widget_two.repaint()
 
-    def on_click(self):
-        print("Button clicked")
-        x_data, y_data = self.open_file()
-        if x_data is not None and y_data is not None:
-            print("x data:", x_data)
-            print("y data:", y_data)
+    def update_count(self):
+        subscriber_count = self.fetch_subscriber_count.subscriber_count
+        current_time = time.time() - self.start_time  # Calculate elapsed time
 
-    def open_file(self):
-        filename = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv)")
-        path = filename[0]
+        self.timestamps.append(current_time)
+        self.subscriber_counts.append(subscriber_count)
 
-        if path:
-            data = pd.read_csv(path)
-            x = data.iloc[:, 0].values
-            y = data.iloc[:, 1].values
-            return x, y
-        else:
-            print("No file selected")
-            return None, None
+        self.plot_widget_two.clear()  # Clear previous plots
+        self.plot_widget_two.plot(self.timestamps, self.subscriber_counts, pen=pg.mkPen(color='g', width=2), symbol='o',
+                                  symbolBrush='w')
 
-    def import_signal(self):
-        pass
+        # Set axis labels
+        self.plot_widget_two.setLabel('left', 'Subscribers')
+        self.plot_widget_two.setLabel('bottom', 'Time')
+
+        # Set Y-axis range to highlight small changes
+        if self.subscriber_counts:
+            y_min = min(self.subscriber_counts) - 10  # Adjust as needed for clarity
+            y_max = max(self.subscriber_counts) + 10
+            self.plot_widget_two.setYRange(y_min, y_max)
+            self.plot_widget_two.setXRange(0, max(self.timestamps) + 5)
+
+        # Format x-axis to show time
+        self.plot_widget_two.getAxis('bottom').setTicks(
+            [[(t, time.strftime("%H:%M:%S", time.gmtime(t))) for t in self.timestamps]])
+
+        # Optional: Format y-axis ticks for clarity
+        self.plot_widget_two.getAxis('left').setTicks(
+            [[(count, str(count)) for count in range(int(y_min), int(y_max) + 1, 5)]])
+
+        self.plot_widget_two.repaint()  # Force a refresh of the plot
 
 
 def main():
